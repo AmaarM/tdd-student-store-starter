465790d66c758c6e169c309963628424c74fb3ce
Amaar Mohamed <codebyamaar@gmail.com>
2022-06-24 03:44:28 -0500

    more styling done

--- student-store-ui/src/components/Hero/Hero.jsx
+++ student-store-ui/src/components/Hero/Hero.jsx
@@ -1,13 +1,12 @@
-import HeroImg from "./hero-img.jpg"
-import "./Hero.css"
+import HeroImg from "./hero-img.jpg";
+import "./Hero.css";
 
-export default function Hero(){
-    return(
-        <div className="hero">
-            <p className="intro">Welcome!</p>
-            <img className="hero-img" src={HeroImg} width={300} height={300}></img>
-            <p className="intro-1">Where all you're shopping needs are met!</p>
-        </div>
-    )
-
+export default function Hero() {
+  return (
+    <div className="hero">
+        <h1 className="intro">Welcome!</h1>
+        <img className="hero-img" src={HeroImg} width={300} height={300}></img>
+        <h1 className="intro-1">Where all you're shopping needs are met!</h1>
+    </div>
+  );
 }
